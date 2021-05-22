f9d8121e5ecb034126b933c79c5892f281108df5
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-22 16:11:22 -0300

    commit 01 alterado

--- 01FuncaoBasico.js
+++ 01FuncaoBasico.js
@@ -1,28 +1,29 @@
-Função possibilidades
+//Funcao possibilidades
 
-// Função em JS é First-Class Object (Citizens)
-// Higher-order function
-// uma função sempre retorna por padrão o valor udefined,
-desde que não tenha o return
+/* Funcao em JS eÂ´ First-Class Object (Citizens)
+   Higher-order function
+   uma funcao sempre retorna por padrï¿½o o valor udefined,
+desde que nao tenha o return
+*/
 
-// criar de forma literal
+//criar de forma literal
 function fun1() { }
 
-// Armazenar em uma variável
+//Armazenar em uma variavel
 const fun2 = function () { }
 
-// Armazenar em um array
+//Armazenar em um array
 const array = [function (a, b) { return a + b }, fun1, fun2]
 console.log(array[0](2, 3))
-// 5 
+//5 
 
-// Armazenar em um atributo de objeto
+//Armazenar em um atributo de objeto
 const obj = {}
 obj.falar = function () { return 'Opa' }
 console.log(obj.falar())
-// Opa
+//Opa
 
-// Passar função como param
+//Passar funcao como param
 function run(fun) {
     fun()
 }
@@ -30,7 +31,7 @@ function run(fun) {
 run(function () { console.log('Executando...') })
 //Executando...
 
-// Um função pode retornar/conter uma função
+// Um funcao pode retornar/conter uma funcao
 function soma(a, b) {
     return function (c) {
         console.log(a + b + c)
