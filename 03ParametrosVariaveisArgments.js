12c48e51a71ce08d8730dba2d59de25a95704064
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-23 12:40:10 -0300

    commit 03 alterado

---
+++ 03ParametrosVariaveisArgments.js
@@ -0,0 +1,20 @@
+//Parametros e variaveis
+
+function soma() {
+    let soma = 0
+    for (i in arguments) { //arguments e´ um array, usado para adicionar parametros e´ funcao
+        soma += arguments[i] //vai somar todos os parametros da funcao
+    }
+    return soma
+}
+
+console.log(soma())
+// 0
+console.log(soma(1))
+// 1
+console.log(soma(1.1, 2.2, 3.3))
+// 6.6
+console.log(soma(1.1, 2.2, "Teste"))
+// 3.30000000003teste
+console.log(soma('a', 'b', 'c'))
+// 0abc
