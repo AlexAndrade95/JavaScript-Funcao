173f4a76437d527462c2dba4b93414c3d9791fed
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-25 19:40:21 -0300

    commit 10 alterado

---
+++ 10ArrowFunction3.js
@@ -0,0 +1,27 @@
+//Arrow Function 3
+
+let comparaComThis = function (param) {
+    console.log(this === param)
+}
+
+comparaComThis(global)
+//true
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+//false
+comparaComThis(obj)
+//true
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+//false // por que arrow funcion no node o this esta dentro do modulo
+comparaComThisArrow(module.exports)
+//true
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
+//false // por que a arrow function e mais forte(nao muda de contexto para o bind) 
+comparaComThisArrow(module.exports)
+//true
