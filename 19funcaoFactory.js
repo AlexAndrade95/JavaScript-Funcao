d418617ca1426d716000fb8d025f267279dded35
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-26 19:43:57 -0300

    commit 19 alterado

---
+++ 19funcaoFactory.js
@@ -0,0 +1,13 @@
+/* funcao factory
+funcao que retorna um objeto */
+
+// Factory simples
+function criarPessoa() {
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa())
+//{ nome: 'Ana', sobrenome: 'Silva' }
