0ebbca3d6572b1c1158abdfadfba449e0f276b36
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-26 19:42:30 -0300

    commit 17 alterado

---
+++ 17ContextoLexico.js
@@ -0,0 +1,15 @@
+//Contexto lexico
+
+const valor = 'Global'
+
+function minhaFuncao() {
+    console.log(valor)
+}
+
+function exec() {
+    const valor = 'Local'
+    minhaFuncao()
+}
+
+exec() //Local ou Global?
+// Global 
