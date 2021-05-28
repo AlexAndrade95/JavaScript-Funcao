4ff63a3135a09e5c86c5985010bca257945876cb
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-28 19:23:04 -0300

    commit 21 alterado

---
+++ 21ClasseVSFactory.js
@@ -0,0 +1,43 @@
+//Classe vs factory
+
+//classe:
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    falar() {
+        console.log(`Meu nome e ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('Joao')
+p1.falar()
+// Meu nome e Joao // da problema no browser pq usa this
+
+//funcao factory:
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome e ${nome}`) 
+    }
+}
+
+const p2 = criarPessoa('Joao')
+p2.falar()
+// Meu nome e Joao // nao da problema no browser pq nao usa this
+
+//factory:
+
+function Pessoa (nome)  {
+	this(nome) = nome
+        this.nome = nome
+    }
+
+    this.falar()= function {
+        console.log(`Meu nome e ${this.nome}`)
+    }
+
+const p1 = new Pessoa('Joao')
+p1.falar()
+// Meu nome e Joao
+
