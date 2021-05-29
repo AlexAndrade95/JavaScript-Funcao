bd339c398faf344a09e0b2a30fa412efad71de6d
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-28 21:04:49 -0300

    commit 13 atualizado 1.2

--- 13Callback2.js
+++ 13Callback2.js
@@ -1,5 +1,5 @@
 /* Callback 2 
-"Chamar de volta" 
+"Chamar de volta", usando filter
 */
 
 const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
@@ -15,16 +15,15 @@ for (let i in notas) {
 console.log(notasBaixas1)
 //[ 6.5, 5.2, 3.6 ]
 
-// Com callback
-const notasBaixas2 = notas.filter(function (nota) {   /* Filter retornara false ou true
-                                                         adicionando o elemento ou nao */
+//Com callback
+const notasBaixas2 = notas.filter(function (nota) {   /*Filter retornara false ou true
+                                                        adicionando o elemento ou nao */
     return nota < 7
 })
-
 console.log(notasBaixas2)
-// [ 6.5, 5.2, 3.6 ]
+//[ 6.5, 5.2, 3.6 ]
+
 const notasMenorQue7 = nota => nota < 7
-// [ 6.5, 5.2, 3.6 ]
 const notasBaixas3 = notas.filter(notasMenorQue7)
 console.log(notasBaixas3)
 //[ 6.5, 5.2, 3.6 ]
